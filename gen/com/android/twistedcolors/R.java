3284b932cc72baabfe4e18d5aa4f6c9a3f2ef060
Kervin Ramen <kervin152@gmail.com>
2010-03-25 23:27:55 +0400

    I created  a UI with some imagebuttons.  I also created an evenlistener for red button.
    Signed-off-by: Kervin Ramen <kervin152@gmail.com>

--- gen/com/android/twistedcolors/R.java
+++ gen/com/android/twistedcolors/R.java
@@ -13,6 +13,12 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int ImageButtonBlue=0x7f050001;
+        public static final int ImageButtonGreen=0x7f050003;
+        public static final int ImageButtonRed=0x7f050000;
+        public static final int ImageButtonYellow=0x7f050002;
+    }
     public static final class layout {
         public static final int main=0x7f030000;
     }
--- src/com/android/twistedcolors/main.java
+++ src/com/android/twistedcolors/main.java
@@ -1,7 +1,13 @@
 package com.android.twistedcolors;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class main extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +15,28 @@ public class main extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // Capture the red button from layout
+        ImageButton imageButtonRed = (ImageButton)findViewById(R.id.ImageButtonRed);
+        // Register the onClick listener with the implementation above
+        imageButtonRed.setOnClickListener(readInput);
+        
+       
+        
     }
+    
+    
+    // Create an anonymous implementation of OnClickListener
+    private OnClickListener readInput = new OnClickListener() {
+        public void onClick(View v) {
+          // Display a message to the user
+        	Context context = getApplicationContext();
+        	int duration = Toast.LENGTH_SHORT;
+        	
+        	Toast toast = Toast.makeText(context, "You clicked Red", duration);
+        	toast.show();	
+        }
+    };
+
+    
 }
